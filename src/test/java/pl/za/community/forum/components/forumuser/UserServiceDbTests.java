16c3ebcc96d011d2636eb5b0b05e28d03a9133c8
k.bt@gmail.com <k.bt@gmail.com>
2024-06-23 00:20:08 +0200

    refactor testcontainer property registering

--- src/test/java/pl/za/community/forum/components/forumuser/UserServiceDbTests.java
+++ src/test/java/pl/za/community/forum/components/forumuser/UserServiceDbTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import pl.za.community.forum.testsetup.DbTests;
@@ -14,6 +13,7 @@ import pl.za.community.forum.testsetup.DbTests;
 @DbTests
 public class UserServiceDbTests extends UserServiceTests {
     @Container
+    @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
             "postgres:16-alpine"
     );
@@ -23,6 +23,7 @@ public class UserServiceDbTests extends UserServiceTests {
 
     @Override
     protected ForumUserService supplyTestedService() {
+        // todo entities should be deleted
         return service;
     }
 
@@ -36,10 +37,4 @@ public class UserServiceDbTests extends UserServiceTests {
         postgres.stop();
     }
 
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
 }
